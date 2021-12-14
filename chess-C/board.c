bb2803855fc0f350cfb66e02629b5217020bfcaf
nico <nicolas.prevost@epita.fr>
2021-12-14 01:08:34 +0100

    board.c

--- chess-C/board.c
+++ chess-C/board.c
@@ -1,19 +1,53 @@
 #include "board.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 char* init_board()
 {
-    char* ret = malloc(64*sizeof(char));
+    char* ret=calloc(64, sizeof(char));
+    ret[9]=129;
+
     return ret;
+}
+
+
+void white () {printf("\033[0;37m");}
+void black () {printf("\033[0;30m");}
+void red () {printf("\033[0;31m");}
+void green () {printf("\033[0;32m");}
+
+char get_team(char c){return c>>7;}
+
+
+void reset () {
+  printf("\033[0m");
 }
 
 void Print(char* board)
 {
+    system("clear");
+    printf("%d====", get_team(board[9]));
+    white();
     for (char i = 0; i < 8; i++)
     {
         for (char j = 0; j < 8; j++)
         {
-            printf("0");
+            if(board[i*8+j])
+            {
+                if(get_team(board[i*8+j]))
+                {
+                    green();
+                }
+                else
+                {
+                    red();
+                }
+                printf("0");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("\n");
     }
--- chess-C/main.c
+++ chess-C/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "board.h"
-//1 2 4 8 16 32 64
-
-char *board = init_board();
-
+//1    2 4 8 16 32 64
+//team          piece
+//
+//
+//
+//
+//
 
 int main()
 {
+    char* board=init_board();
+    init_board(board);
     Print(board);
 }
